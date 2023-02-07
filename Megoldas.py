9ca7838d2a4c796f8ecc47081324df77e179760d
GerencserOliver <gerencser.oliver@students.jedlik.eu>
2023-02-07 09:22:31 +0100

    Megoldas osztaly

---
+++ Megoldas.py
@@ -0,0 +1,11 @@
+from Furdo import Furdo
+
+
+class Megoldas:
+    furdok: list[Furdo]
+
+    def __init__(self, forras: str):
+        self.furdok = []
+        with open(forras, 'r', encoding='utf-8')as file:
+            for sor in file.read().splitlines():
+                self.furdok.append(Furdo(sor))
