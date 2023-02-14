eee810e9f0f3d288a4fe7c00030ffd80e4626a3f
GerencserOliver <gerencser.oliver@students.jedlik.eu>
2023-02-14 07:56:54 +0100

    Harmadik feladat kész Fixes #18

--- Megoldas.py
+++ Megoldas.py
@@ -25,6 +25,20 @@ class Megoldas:
     def kiiras_oraban(self, vendeg: Furdo) -> str:
         return f'{vendeg.ki_ora}:{vendeg.ki_perc}:{vendeg.ki_mp}'
 
+    def vendegek(self) -> int:
+        furdo_helyek: list[int] = []
+        ketto_hasznalatos_vendeg: int = 0
+        elozo_vendeg: Furdo = self.furdok[0]
+        for vendeg in self.furdok:
+            if elozo_vendeg.azonosito != vendeg.azonosito:
+                if len(furdo_helyek) == 2:
+                    ketto_hasznalatos_vendeg += 1
+                furdo_helyek.clear()
+            if vendeg.furdo_azonosito not in furdo_helyek:
+                furdo_helyek.append(vendeg.furdo_azonosito)
+            elozo_vendeg = vendeg
+        return ketto_hasznalatos_vendeg
+
     def __init__(self, forras: str):
         self.furdok = []
         with open(forras, 'r', encoding='utf-8')as file:
